505ca388e36ce43aab5fa72d7b8b89f5a24034f0
Manjeet <yadavmanjeet629@gmail.com>
2025-06-20 18:37:27 +0530

    feat: Implement user authentication and file upload functionality

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,167 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
+
+const registerUser = async(req, res) => {
+
+    const { email, fullName, gender, contactNumber, password, location } = req.body
+
+    let avatarLocalPath;
+    if (req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
+        avatarLocalPath = req.files.avatar[0].path
+    }
+
+    if( 
+        [ email, fullName, password, gender].some((field) => (
+        field?.trim() === "" || !field))
+    ) {
+        throw new ApiError(400, `email, fullName, password all are required`)
+    }
+
+    if(gender !== "Male" && gender !== "Female") {
+        throw new ApiError(400, `gender must be either "Male" or "Female"`)
+    }
+    
+    const existingUser = await User.findOne({email})
+    
+    if(existingUser) {
+        throw new ApiError(409, "User with same email already exists")
+    }
+    
+    let avatar = ""
+    if(avatarLocalPath) {
+        avatar = await uploadOnCloudinary(avatarLocalPath)
+    }
+
+    const userLocation = location ? { type: "Point", coordinates: location } : { type: "Point", coordinates: [0, 0] };
+
+    const user = await User.create({
+        email,
+        fullName,
+        gender,
+        contactNumber,
+        avatar: avatar?.url || "http://localhost:8000/images/default_avatar.jpg",
+        location: userLocation,
+        password        
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser) {
+        throw new ApiError(500, "Internal error while registering new user")
+    }
+
+    return res
+    .status(200)
+    .json({
+        user: createdUser,
+        message: "User Created Successfully"
+    })
+
+}
+
+const generateAccessAndRefreshToken = async(userId) => {
+
+    try {
+
+        const user = await User.findById(userId)
+        
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+    
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+    
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access and refresh tokens")        
+    }
+    
+}
+
+const secureCookieWithExpiry = {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30) // 30 days
+}
+
+const loginUser = async(req, res) => {
+
+    const { email, password } = req.body
+    
+    if(!email) {
+        throw new ApiError(400, "email is required")
+    }
+
+    if(!password) {
+        throw new ApiError(400, "password field cannot be empty")
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user) {
+        throw new ApiError(404, "user does not exists")
+    }
+
+    const isValidPassword = await user.isPasswordCorrect(password)
+    if(!isValidPassword) {
+        throw new ApiError(400, "Incorrect Password")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = user.toObject()
+    delete loggedInUser.password
+    delete loggedInUser.refreshToken
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, secureCookieWithExpiry)
+    .cookie("refreshToken", refreshToken, secureCookieWithExpiry)
+    .json({
+        user: loggedInUser,
+        message: "User Logged In Successfully"
+    })
+
+}
+
+const logoutUser = async(req, res) => {
+
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const secureCookie = {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", secureCookie)
+    .clearCookie("refreshToken", secureCookie)
+    .json({
+        message: "User logged Out"
+    })
+
+}
+
+
+export { 
+    registerUser,
+    loginUser,
+    logoutUser,
+}
